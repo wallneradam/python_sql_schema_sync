a871ecbce9de039b766f57f6d82697239397514f
wallneradam <adam.wallner@gmail.com>
2019-11-14 09:21:57 +0100

    Better regex for defaults

--- schema_sync.py
+++ schema_sync.py
@@ -259,14 +259,14 @@ def split_table_schema(table_name: str, sql: str, *, ignore_increment: bool = Tr
         nonlocal bottom
 
         # Add default sizes if not specified to make them comparable
-        line = re.sub(r"(\sINT)\s*(?!\()", r"\1(11) ", line, flags=re.I)
-        line = re.sub(r"(\sTINYINT)\s*(?!\()", r"\1(3) ", line, flags=re.I)
-        line = re.sub(r"(\sSMALLINT)\s*(?!\()", r"\1(6) ", line, flags=re.I)
-        line = re.sub(r"(\sBIGINT)\s*(?!\()", r"\1(20) ", line, flags=re.I)
-        line = re.sub(r"(\sVARCHAR)\s*(?!\()", r"\1(255) ", line, flags=re.I)
-        line = re.sub(r"(\sDATETIME)\s*(?!\()", r"\1(6) ", line, flags=re.I)
+        line = re.sub(r"(\sINT)\s*(?![(\w])", r"\1(11) ", line, flags=re.I)
+        line = re.sub(r"(\sTINYINT)\s*(?![(\w])", r"\1(3) ", line, flags=re.I)
+        line = re.sub(r"(\sSMALLINT)\s*(?![(\w])", r"\1(6) ", line, flags=re.I)
+        line = re.sub(r"(\sBIGINT)\s*(?![(\w])", r"\1(20) ", line, flags=re.I)
+        line = re.sub(r"(\sVARCHAR)\s*(?![(\w])", r"\1(255) ", line, flags=re.I)
+        line = re.sub(r"(\sDATETIME)\s*(?![(\w])", r"\1(6) ", line, flags=re.I)
         # Bool is tinyint in MySQL
-        line = re.sub(r"\sBOOL\s(?!\()", r" TINYINT(1) ", line, flags=re.I)
+        line = re.sub(r"\sBOOL\s(?![(\w])", r" TINYINT(1) ", line, flags=re.I)
 
         # Remove DEFAULT NULL, which is the default, to be easier to compare
         try:
